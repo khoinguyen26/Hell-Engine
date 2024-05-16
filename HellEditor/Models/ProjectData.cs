9397868c53e34e6659fe839b11a497fb4de87202
khoinguyen26 <khoinguyen261101@gmail.com>
2024-05-16 20:38:38 +0700

    add ProjectData models

---
+++ HellEditor/Models/ProjectData.cs
@@ -0,0 +1,16 @@
+﻿using HellEditor.ViewModel;
+using System.Runtime.Serialization;
+
+namespace HellEditor.Models
+{
+    public class ProjectData
+    {
+        [DataMember]
+        public string ProjectName { get; set; }
+        [DataMember]
+        public string ProjectPath { get; set; }
+        [DataMember]
+        public DateTime Date { get; set; }
+        public string FullPath { get => $"{ProjectPath}{ProjectName}{Project.Extension}"; }
+    }
+}
---
+++ HellEditor/Models/ProjectDataList.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace HellEditor.Models
+{
+    [DataContract]
+    public class ProjectDataList
+    {
+        [DataMember]
+        public List<ProjectData> Projects { get; set; }
+    }
+}
