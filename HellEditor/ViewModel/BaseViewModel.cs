9327cd922e488112f9d65602e338583e525b91a4
khoinguyen26 <khoinguyen261101@gmail.com>
2024-05-16 23:26:04 +0700

    fix warning

--- HellEditor/ViewModel/BaseViewModel.cs
+++ HellEditor/ViewModel/BaseViewModel.cs
@@ -6,7 +6,7 @@ namespace HellEditor.ViewModel
     [DataContract(IsReference = true)]
     public class BaseViewModel : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
 
         protected void OnPropertyChanged(string propertyName)
         {
