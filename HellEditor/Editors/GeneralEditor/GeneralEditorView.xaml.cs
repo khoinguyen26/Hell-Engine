fee456585d7dede8b69eeb41d26b6a7a6145a929
khoinguyen26 <khoinguyen261101@gmail.com>
2024-05-20 23:39:42 +0700

    add general editor and project layout

---
+++ HellEditor/Editors/GeneralEditor/GeneralEditorView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace HellEditor.Editors
+{
+    /// <summary>
+    /// Interaction logic for GeneralEditorView.xaml
+    /// </summary>
+    public partial class GeneralEditorView : UserControl
+    {
+        public GeneralEditorView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HellEditor/Editors/GeneralEditor/ProjectLayoutView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using HellEditor.ViewModel;
+using System.Windows.Controls;
+
+namespace HellEditor.Editors
+{
+    /// <summary>
+    /// Interaction logic for ProjectLayoutView.xaml
+    /// </summary>
+    public partial class ProjectLayoutView : UserControl
+    {
+        public ProjectLayoutView()
+        {
+            InitializeComponent();
+        }
+
+        private void OnAddScene_Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var vm = DataContext as Project;
+            vm.AddScene("New Scene" + vm.Scenes.Count);
+        }
+    }
+}
